4dcb60e541644820445865158bb035447533b6f4
bruno felipe <brunofelipehp8@gmail.com>
2022-09-16 23:39:01 -0300

    to be continued

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,14 +1,34 @@
 import express from 'express';
+import { PrismaClient } from '@prisma/client';
 
 const app = express();
+const prisma = new PrismaClient({
+  log: ['query']
+});
+
+app.get('/games', async(request, response) => {
+  const games = await prisma.game.findMany();
+
+  return response.json(games);
+});
+
+app.post('/ads', (request, response) => {
+  return response.status(201).json([]);
+})
 
 
-app.get('./ads', (request, response) => {
+app.get('/games/:id/ads', (request, response) => {
   return response.json([
     {id: 1, name: 'anúncio 1'},
     {id: 2, name: 'anúncio 2'},
     {id: 3, name: 'anúncio 3'},
   ])
+})
+
+
+app.get('/ads/:id/discord', (request, response) => {
+ // const adId = request.params.id;
+    return response.json([])
 })
 
 app.listen(3333)
